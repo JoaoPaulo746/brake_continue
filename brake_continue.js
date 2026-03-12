000f9b4924bae0ca6eaec487bc4ce6ed6442e2c6
JoaoPaulo746 <joao.ferreira.araujo06@escola.pr.gov.br>
2026-03-12 08:57:45 -0300

    Add files via upload

---
+++ brake_continue.js
@@ -0,0 +1,14 @@
+let carros_estacionados = 0;
+const limitemaximo = 8;
+    console.log("---iniciando verificação---");
+for(let vaga=1;vaga<=10;vaga++){
+    if(vaga===4 || vaga===7){
+        console.log(`vaga ${vaga}: [INTERDITADA]- Pulando `);
+        continue;}
+        carros_estacionados++;
+        console.log(`vaga ${vaga}: [LIVRE] -> CARRO ESTACIONADO! (TOTAL:${carros_estacionados})`);
+        if(carros_estacionados===limitemaximo){
+            console.log("---LIMITE ATINGINDO! FECHAR PORTÃO---");
+            break}
+}
+console.log("RELATÓRIO FINAL: OPERAÇÃO DE ENTRADA ENCERRADA");
